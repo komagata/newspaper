dad138d3064b3427179b5d42bc90f64bdcfee3f3
komagata <komagata@gmail.com>
2022-04-29 18:22:03 +0900

    Implemented

--- lib/newspaper.rb
+++ lib/newspaper.rb
@@ -5,18 +5,26 @@ require_relative "newspaper/version"
 module Newspaper
   class Error < StandardError; end
 
+  @event_bus = {}
+
   class << self
-    def publish(event, payload)
-    end
+    attr_reader :event_bus
 
     def subscribe(event, subscriber)
+      @event_bus[event] = subscriber
+    end
+
+    def publish(event, payload)
+      subscriber = @event_bus[event]
+      subscriber.call(payload)
     end
 
     def clear(event)
+      @event_bus.delete event
     end
 
     def clear_all
-
+      @event_bus = {}
     end
   end
 end
--- test/newspaper_test.rb
+++ test/newspaper_test.rb
@@ -3,13 +3,34 @@
 require "test_helper"
 
 class NewspaperTest < Test::Unit::TestCase
-  test "VERSION" do
-    assert do
-      ::Newspaper.const_defined?(:VERSION)
+  class UserSignupNotifier
+    def call(payload)
+      $MESSAGES << 'user signup!'
     end
   end
 
-  test "something useful" do
-    assert_equal("expected", "actual")
+  $MESSAGES = []
+
+  setup do
+    $MESSAGES = []
+  end
+
+  test ".subscribe" do
+    notifier = UserSignupNotifier.new
+    Newspaper.subscribe(:user_create, notifier)
+    assert_equal notifier, Newspaper.event_bus[:user_create]
+  end
+
+  test ".publish" do
+    Newspaper.subscribe(:user_create, UserSignupNotifier.new)
+    Newspaper.publish(:user_create, { name: 'foo' })
+    assert_equal 'user signup!', $MESSAGES.last
+  end
+
+  test ".event_bus" do
+    notifier = UserSignupNotifier.new
+    Newspaper.subscribe(:user_create, notifier)
+    event_bus = { user_create: notifier }
+    assert_equal event_bus, Newspaper.event_bus
   end
 end
